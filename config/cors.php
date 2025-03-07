3a479887bc6ab645b181d955d52fff4286170b0b
Edinson Fernandez <142480192+AnotherEz@users.noreply.github.com>
2025-03-07 04:05:47 -0500

    beggin

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Asegúrate de usar la URL exacta del frontend
+    'allowed_origins' => ['http://localhost:5173', 'https://venta-react.vercel.app'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
